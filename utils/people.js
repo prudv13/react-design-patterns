ad8e44ecdcbdd0a96cb691a3dae5b47ce6b47afc
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-29 15:13:30 +0530

    added utils data

---
+++ utils/people.js
@@ -0,0 +1,22 @@
+const people = [
+    {
+        name: 'John Doe',
+        age: 54,
+        hairColor: 'brown',
+        hobbies: ['swimming', 'bicycling', 'video games'],
+    },
+    {
+        name: 'Brenda Smith',
+        age: 33,
+        hairColor: 'black',
+        hobbies: ['golf', 'mathematics'],
+    },
+    {
+        name: 'Jane Garcia',
+        age: 27,
+        hairColor: 'blonde',
+        hobbies: ['biology', 'medicine', 'gymnastics'],
+    }
+];
+
+export default people;
