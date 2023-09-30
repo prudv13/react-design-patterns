1c8fb0eb69a5ab45d76b1b157c97b84881c0c1e0
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-30 21:23:41 +0530

    Controlled Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LayoutComponent from './design/01-Layout Components/LayoutComponent';
 import UncontrolledForm from './design/02-Controlled-Uncontrolled/UncontrolledForm';
 import ControlledForm from './design/02-Controlled-Uncontrolled/ControlledForm';
 import UncontrolledModal from './design/02-Controlled-Uncontrolled/UncontrolledModal';
+import ControlledModal from './design/02-Controlled-Uncontrolled/ControlledModal';
 
 const App = () => {
+
+  const [shouldShowModal, setShouldShowModal] = useState(false);
   return (
     <center>
       {/* <LayoutComponent /> */}
       {/* <UncontrolledForm /> */}
       {/* <ControlledForm /> */}
-      <UncontrolledModal>
+
+      {/* <UncontrolledModal>
         <p>This is Uncontrolled Modal</p>
-      </UncontrolledModal>
+        </UncontrolledModal> */}
+      
+      <button 
+        className='btn btn-dark my-4' 
+        onClick={() => setShouldShowModal(!shouldShowModal)}
+      >
+      {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+      </button>
+      <ControlledModal 
+        shouldShow={shouldShowModal}
+        onRequestClose={() => setShouldShowModal(false)}
+      >
+        <p>This is Controlled Modal</p>
+      </ControlledModal>
     </center>
   )
 }
